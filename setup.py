c424189b2abc39ab8962abf74a2e9983e8e80213
cle-b <cle@tictac.pm>
2019-09-28 16:46:39 +0200

    add py36 build

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="niobium",
-    version="0.3.0",
+    version="0.3.1",
     author="cle-b",
     author_email="cle@tictac.pm",
     description="Niobium extends the Python Selenium client with nice features",
@@ -17,11 +17,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/cle-b/niobium",
     packages=setuptools.find_packages(),
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     install_requires=["selenium", "opencv-python", "pillow"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
     ],
 )
